dec7bd76b56a4e74fc55be6bec3cda5129bfdb05
kavyamohan-24 <kavyamohan2005@gmail.com>
2024-07-08 21:32:37 +0530

    metro app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
 import HomePage from './components/HomePage';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import AboutPage from './components/AboutPage';
 
 function App() {
   return (
-    <div>
-      <HomePage/>
-    </div>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<HomePage/>}/>
+      <Route path="/about" element={<AboutPage/>}/>
+    </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/AboutPage.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const AboutPage = () => {
+  return (
+    <div>
+      <div className="container">
+        <div className="row">
+            <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+            The Kochi Metro App is a state-of-the-art mobile application developed to enhance the commuting experience of Kochi Metro users. Created by Kochi Metro Rail Limited (KMRL) in collaboration with technology partners and developers, the app aims to provide a comprehensive and user-friendly platform for accessing a wide range of metro-related services and information.
+            </div>
+        </div>
+        <br />
+        <div className="row">
+          <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+          <img src="https://themetrorailguy.com/wp-content/uploads/2015/07/kochi2.jpg" class="img-fluid" alt="..."/>
+          <p>The motivation behind the development of the Kochi Metro App stems from a commitment to transforming the urban commuting experience and addressing key challenges faced by metro users. Recognizing the need for a convenient and efficient solution, Kochi Metro Rail Limited (KMRL) aimed to streamline travel by integrating essential services into a single, user-friendly platform. The app promotes the use of public transportation by offering real-time updates, easy ticketing, and comprehensive journey planning, thereby reducing traffic congestion and environmental impact. Additionally, it enhances operational efficiency and user satisfaction, providing instant access to crucial information and support. By embracing digital innovation, KMRL sought to make public transportation more attractive, sustainable, and responsive to the needs of Kochi's diverse population, ultimately fostering a more connected and eco-friendly urban environment.</p>
+          <h3>History behind </h3>
+          The history behind the development of the Kochi Metro App is rooted in the evolution of urban transportation needs and technological advancements in Kochi. As the city grappled with increasing traffic congestion and environmental concerns, the inception of the Kochi Metro system marked a pivotal moment in its infrastructure development. Recognizing the transformative potential of digital solutions, Kochi Metro Rail Limited (KMRL) embarked on the creation of a mobile application to complement and enhance the metro experience. This initiative was driven by a desire to not only modernize public transportation but also to improve accessibility, convenience, and efficiency for commuters.
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default AboutPage
