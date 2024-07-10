46ab824c10c1eca1b6fd6bad60f979c8a496cfd1
kavyamohan-24 <kavyamohan2005@gmail.com>
2024-07-10 21:44:13 +0530

    metro app

--- src/components/ServicePage.jsx
+++ src/components/ServicePage.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
+import Navbar from './Navbar'
 
 const ServicePage = () => {
   return (
     <div>
+      <Navbar/>
       <div className="container">
         <div className="row">
             <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
