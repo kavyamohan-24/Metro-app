674a57fc1c7577137dfe38a36fd182eeb5a9803f
kavyamohan-24 <kavyamohan2005@gmail.com>
2024-07-10 20:17:00 +0530

    metro app

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import HomePage from './components/HomePage';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import AboutPage from './components/AboutPage';
 import ServicePage from './components/ServicePage';
+import ContactPage from './components/ContactPage';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <Route path="/" element={<HomePage/>}/>
       <Route path="/about" element={<AboutPage/>}/>
       <Route path="/service" element={<ServicePage/>}/>
+      <Route path="/contact" element={<ContactPage/>}/>
     </Routes>
     </BrowserRouter>
   );
---
+++ src/components/ContactPage.jsx
@@ -0,0 +1,46 @@
+import React from 'react'
+
+const ContactPage = () => {
+  return (
+    <div>
+      <div className="container">
+        <div className="row">
+            <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                <div className="row g-3">
+                    <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                        <label htmlFor="" className="form-label">Name</label>
+                        <input type="text" className="form-control" />
+                    </div>
+                    <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                        <label htmlFor="" className="form-label">Email id</label>
+                        <input type="text" className="form-control" />
+                    </div>
+                    <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                        <label htmlFor="" className="form-label">Subject</label>
+                        <input type="text" className="form-control" />
+                    </div>
+                    <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                        <label htmlFor="" className="form-label">Message</label>
+                        <input type="text" className="form-control" />
+                    </div>
+                    <h5>Other contact details <h6>(optional)</h6></h5>
+                    <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                        <label htmlFor="" className="form-label">Phone No</label>
+                        <input type="text" className="form-control" />
+                    </div>
+                    <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                        <label htmlFor="" className="form-label">Address</label>
+                        <textarea name="" id="" cols="30" rows="10" className="form-control"></textarea>
+                    </div>
+                    <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                        <button className="btn btn-success">Submit</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default ContactPage
